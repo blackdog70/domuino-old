a3730170e091f2789f6ef45585c14278f1807e57
blackdog70 <sbonisolo@gmail.com>
2016-11-05 09:52:01 +0100

    Added callback to channel and some refactoring

--- Domuino.cpp
+++ Domuino.cpp
@@ -12,17 +12,14 @@
 
 Domuino::Domuino(uint8_t node_id, uint32_t baudrate) : DomuNet(node_id, baudrate) {
 	hub_id = 1;
-	hb_timeout.timer = millis();
 	hb_timeout.value = HB_TIMING;
 	channel_id = 0;
-	hbt.code = QUERIES::HBT;
 }
 
 void Domuino::begin() {
 	pinMode(en485, OUTPUT);
 	rs485.flush();
 	payload_start start;
-	start.code = QUERIES::START;
 	start.node_id = node_id;
 	send(hub_id, &start, sizeof(base_payload));
 }
@@ -48,8 +45,8 @@ uint8_t Domuino::parse_packet(void* payload) {
 				switch(CONFIG_DATA->parameter) {
 					// CONFIG HEARTBIT FREQUENCY
 					case PARAMETERS::HBT:
-						if(CONFIG_DATA->value >= 1)
-							hb_timeout.value = CONFIG_DATA->value * 1000UL;
+						if(SIMPLE_PARAMETER->value >= 1)
+							hb_timeout.value = SIMPLE_PARAMETER->value * 1000UL;
 						PAYLOAD_CODE = ANSWERS::ACK;
 						size = PAYLOAD_SIZE;
 						break;
@@ -58,7 +55,7 @@ uint8_t Domuino::parse_packet(void* payload) {
 			break;
 		case QUERIES::MEM: {
 				payload_mem mem;
-				mem.code = ANSWERS::RAM;
+
 				mem.memory = freeMemory();
 				size = sizeof(payload_mem);
 				memcpy(payload, &mem, size);
@@ -73,7 +70,11 @@ void Domuino::run() {
 		unsigned char payload_[MAX_DATA_SIZE];
 
 		memcpy(payload_, &packet_in.payload, packet_in.core.data_size);
+		// Parse a packet and get an answer
 		packet_in.core.data_size = parse_packet(&payload_);
+		if (packet_in.core.data_size < 1)
+			// Don't send an answer if message is unrecognized
+			return;
 		packet_in.core.dest = packet_in.core.source;
 		memcpy(&packet_in.payload, payload_, packet_in.core.data_size);
 #ifdef DEBUG
@@ -93,41 +94,32 @@ void Domuino::run() {
 		}
 }
 
-// TODO: Trovare nome metodo più adeguato
-void Domuino::refresh_channel(data_channel* data) {
-	data->sending = 1;
-}
-
 void Domuino::update_hub(data_channel* data) {
 	if ((data == NULL) || (data->t == NULL))
 		return;
-	// If data->t->value == 0 is a single shot channel otherwise is an interval channel
-	if (data->t->value > 0)
-		data->sending = ((millis() - data->t->timer) > data->t->value);
-	if(data->sending) {
-		if (send(hub_id, data->data, data->size)) {
-			data->t->timer = millis();
-			data->sending = 0;
-			data->retry = 0;
-			parse_ack(&packet_in);
-		}
-		else
-			if (data->retry++ > MAX_RETRY) {
+	if((millis() - data->t->timer) > data->t->value) {
+		if (data->function()) {
+			if (send(hub_id, data->data, data->size)) {
 				data->t->timer = millis();
-				data->sending = 0;
 				data->retry = 0;
+				parse_ack(&packet_in);
 			}
+			else
+				if (data->retry++ > MAX_RETRY) {
+					data->t->timer = millis();
+					data->retry = 0;
+				}
+		}
 	}
 }
 
-data_channel* Domuino::channel(void* data, uint8_t size, timeout* t) {
+void Domuino::channel(void* data, uint8_t size, timeout* t, ifuncptr fn) {
 	if (channel_id < MAX_CHANNELS && size <= MAX_DATA_SIZE) {
 		channels[channel_id].data = data;
 		channels[channel_id].size = size;
 		channels[channel_id].t = t;
 		channels[channel_id].retry = 0;
+		channels[channel_id].function = fn;
 		channel_id++;
-		return &channels[channel_id];
 	}
-	return NULL;
 }
--- Domuino.h
+++ Domuino.h
@@ -14,7 +14,10 @@
 #define PAYLOAD_CODE ((base_payload*)payload)->code
 #define PAYLOAD_SIZE sizeof(base_payload)
 #define CONFIG_DATA ((payload_config*)payload)
+#define SIMPLE_PARAMETER ((simple_parameter*)CONFIG_DATA->value)
+#define OUT_PARAMETERS ((out_parameters*)CONFIG_DATA->value)
 #define HUB_DATA ((payload_hub*)payload)
+#define SWITCH_DATA ((payload_switch*)payload)
 #define MAX_CHANNELS 3
 #define MAX_RETRY 3
 
@@ -27,7 +30,7 @@ public:
 	static const unsigned char ERR = 0x7f;
 	static const unsigned char ACK = 0x7e;
 	static const unsigned char PONG = 0x7d;
-	static const unsigned char RAM = 0x7c;
+	static const unsigned char MEM = 0x7c;
 	static const unsigned char HBT = 0x7b;
 	static const unsigned char START = 0x7a;
 	// DEVICE
@@ -65,30 +68,96 @@ public:
 struct timeout {
 	unsigned long timer;
 	unsigned long value;
+	timeout():timer(millis()), value(0) {};
 };
+
+typedef uint8_t (*ifuncptr)();
 
 struct data_channel {
 	void* data;
 	uint8_t size;
 	timeout* t;
 	uint8_t retry;
-	uint8_t sending;
+	ifuncptr function;
 };
 
+// FIXME: Suddividere strutture tra QUERY e ANSWER
+
+/*
+ * PARAMETERS
+ */
+struct simple_parameter {
+	char value;
+};
+
+struct out_parameters {
+	char pin;
+	char value;
+	char negate;
+};
+
+/*
+ * INCOMING PAYLOAD
+ */
 struct payload_config : base_payload {
 	uint8_t parameter;
-	char value;
+	char *value;
+	payload_config():base_payload(QUERIES::CONFIG) {};
 };
 
 struct payload_hub : base_payload {
 	uint8_t node_id;
+	payload_hub():base_payload(QUERIES::HUB) {};
 };
 
 struct payload_mem : base_payload {
 	uint16_t memory;
+	payload_mem():base_payload(ANSWERS::MEM) {};
 };
 
-struct payload_start : payload_hub {};
+struct payload_start : base_payload {
+	uint8_t node_id;
+	payload_start():base_payload(QUERIES::START) {};
+};
+
+struct payload_hbt : base_payload {
+	payload_hbt():base_payload(QUERIES::HBT) {};
+};
+
+struct payload_lux : base_payload {
+	int value;
+	payload_lux():base_payload(QUERIES::LUX) {};
+};
+
+struct payload_pir : base_payload {
+	char state;
+	payload_pir():base_payload(QUERIES::PIR) {};
+};
+
+struct payload_switch : base_payload {
+	char pin;
+	char value;
+	payload_switch():base_payload(QUERIES::SWITCH) {};
+};
+
+//add your function definitions for the project domuwin here
+#define NUM_EMS 2
+#define NUM_IO 2
+
+struct payload_inps : base_payload {
+	uint8_t state[NUM_IO];
+	payload_inps():base_payload(QUERIES::SWITCH) {};
+};
+
+struct payload_outs : base_payload {
+	uint8_t state[NUM_IO];
+	payload_outs():base_payload(QUERIES::BINARY_OUT) {};
+};
+
+struct payload_ems : base_payload {
+	double value[NUM_EMS];
+	payload_ems():base_payload(QUERIES::EMS) {};
+};
 
 class Domuino: public DomuNet {
 public:
@@ -96,17 +165,16 @@ public:
 	virtual ~Domuino() {};
 	void run();
 	void begin();
+	void channel(void* data, uint8_t size, timeout* t, ifuncptr fn);
 
 protected:
 	uint8_t hub_id;
 	timeout hb_timeout;
 	uint8_t channel_id;
 	data_channel channels[MAX_CHANNELS];
-	base_payload hbt;
+	payload_hbt hbt;
 
-	uint8_t parse_packet(void* payload);
-	data_channel* channel(void* data, uint8_t size, timeout* t);
-	void refresh_channel(data_channel* data);
+	virtual uint8_t parse_packet(void* payload);
 
 private:
 	void update_hub(data_channel* data);
